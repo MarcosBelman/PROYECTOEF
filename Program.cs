6ffcad9cabe05a7fdfb7650b39ac9fce1dd0d9a1
MarcosBelman <marcos_belman@hotmail.com>
2023-01-25 12:34:15 -0600

    ProgramaFinal

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
 
 app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext)=>
 {
-    return Results.Ok(dbContext.Tareas.Include(p => p.Categoria).Where(p => p.PrioridadTarea == PROYECTOEF.Models.Prioridad.Baja));
+    return Results.Ok(dbContext.Tareas.Include(p => p.Categoria));
 });
 
 app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea) =>
@@ -34,8 +34,44 @@ app.MapPost("/api/tareas", async ([FromServices] TareasContext dbContext, [FromB
     await dbContext.AddAsync(tarea);
     //await dbContext.Tareas.AddAsync(tarea);
 
-    return Results.Ok(dbContext.Tareas.Include(p => p.Categoria).Where(p => p.PrioridadTarea == PROYECTOEF.Models.Prioridad.Baja));
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok();
 });
+
+app.MapPut("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromBody] Tarea tarea, [FromRoute] Guid id) =>
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+
+    if (tarea != null)
+    {
+        tareaActual.CategoriaId = tarea.CategoriaId;
+        tareaActual.Titulo = tarea.Titulo;
+        tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+        tareaActual.Descripcion = tarea.Descripcion;
+        
+        await dbContext.SaveChangesAsync();
+        
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
+});
+
+app.MapDelete("/api/tareas/{id}", async ([FromServices] TareasContext dbContext, [FromRoute] Guid id) =>
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+
+    if (tareaActual!=null) 
+    {
+        dbContext.Remove(tareaActual);
+        await dbContext.SaveChangesAsync();
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
+});
+
 
 app.Run();
 
